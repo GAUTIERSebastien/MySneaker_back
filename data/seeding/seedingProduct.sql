a7a66a43ec18ea1d375b6a1f1b44b6bfb3f6406c
alexandre-cabarez <alexandre.cabarez@gmail.com>
2023-02-16 16:40:45 +0100

    seeding!!!!

---
+++ data/seeding/seedingProduct.sql
@@ -0,0 +1,83 @@
+INSERT INTO
+    "product" (
+        "title",
+        "description",
+        "brand",
+        "price",
+        "image"
+    )
+VALUES (
+        'T-shirts N°1',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Minima fugit ipsa neque ea vero magnam maxime voluptatum 
+        odit ducimus, qui repellendus, voluptatem dolorem reiciendis 
+        tenetur quo sit. Dolorem, molestias et! Lorem ipsum, dolor sit 
+        amet consectetur adipisicing elit. Minima fugit ipsa neque ea 
+        vero magnam maxime voluptatum odit ducimus, qui repellendus, 
+        voluptatem dolorem reiciendis tenetur quo sit. Dolorem, molestias et!',
+        'Nike',
+        '19.00',
+        '../../../assets/products/f1.jpg'
+    ), (
+        'T-shirts N°2',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Minima fugit ipsa neque ea vero magnam maxime voluptatum 
+        odit ducimus, qui repellendus, voluptatem dolorem reiciendis 
+        tenetur quo sit. Dolorem, molestias et! Lorem ipsum, dolor sit 
+        amet consectetur adipisicing elit. Minima fugit ipsa neque ea 
+        vero magnam maxime voluptatum odit ducimus, qui repellendus, 
+        voluptatem dolorem reiciendis tenetur quo sit. Dolorem, molestias et!',
+        'Adidas',
+        '29.00',
+        '../../../assets/products/f1.jpg'
+    ),
+(
+        'T-shirts N°3',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Minima fugit ipsa neque ea vero magnam maxime voluptatum 
+        odit ducimus, qui repellendus, voluptatem dolorem reiciendis 
+        tenetur quo sit. Dolorem, molestias et! Lorem ipsum, dolor sit 
+        amet consectetur adipisicing elit. Minima fugit ipsa neque ea 
+        vero magnam maxime voluptatum odit ducimus, qui repellendus, 
+        voluptatem dolorem reiciendis tenetur quo sit. Dolorem, molestias et!',
+        'Hollister',
+        '39.00',
+        '../../../assets/products/f1.jpg'
+    ), (
+        'T-shirts N°4',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Minima fugit ipsa neque ea vero magnam maxime voluptatum 
+        odit ducimus, qui repellendus, voluptatem dolorem reiciendis 
+        tenetur quo sit. Dolorem, molestias et! Lorem ipsum, dolor sit 
+        amet consectetur adipisicing elit. Minima fugit ipsa neque ea 
+        vero magnam maxime voluptatum odit ducimus, qui repellendus, 
+        voluptatem dolorem reiciendis tenetur quo sit. Dolorem, molestias et!',
+        'Abercrombie',
+        '49.00',
+        '../../../assets/products/f1.jpg'
+    ),
+(
+        'T-shirts N°5',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Minima fugit ipsa neque ea vero magnam maxime voluptatum 
+        odit ducimus, qui repellendus, voluptatem dolorem reiciendis 
+        tenetur quo sit. Dolorem, molestias et! Lorem ipsum, dolor sit 
+        amet consectetur adipisicing elit. Minima fugit ipsa neque ea 
+        vero magnam maxime voluptatum odit ducimus, qui repellendus, 
+        voluptatem dolorem reiciendis tenetur quo sit. Dolorem, molestias et!',
+        'Zara',
+        '59.00',
+        '../../../assets/products/f1.jpg'
+    ), (
+        'T-shirts N°6',
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Minima fugit ipsa neque ea vero magnam maxime voluptatum 
+        odit ducimus, qui repellendus, voluptatem dolorem reiciendis 
+        tenetur quo sit. Dolorem, molestias et! Lorem ipsum, dolor sit 
+        amet consectetur adipisicing elit. Minima fugit ipsa neque ea 
+        vero magnam maxime voluptatum odit ducimus, qui repellendus, 
+        voluptatem dolorem reiciendis tenetur quo sit. Dolorem, molestias et!',
+        'Zara',
+        '69.00',
+        '../../../assets/products/f1.jpg'
+    );
--- migrations/deploy/init.sql
+++ migrations/deploy/init.sql
@@ -23,7 +23,7 @@ CREATE TABLE
             email ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'
         ),
         CONSTRAINT "french_phone" CHECK (
-            phone ~* '^(0|+33)[1-9]( ?\d{2}){4}$'
+            phone ~* '^[0-9]{10}$'
         ),
         CONSTRAINT "firstname_and_firstname_without_number" CHECK (
             "firstname" ~* '^[a-zA-ZÀ-ÿ-]+$'
