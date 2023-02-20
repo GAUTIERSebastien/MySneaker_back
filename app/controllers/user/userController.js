e0761a065822a736a954563ecc73bb89b938d955
alexandre-cabarez <alexandre.cabarez@gmail.com>
2023-02-20 14:35:20 +0100

    modification de la gestion des message d erreur rajout de bcrypt et email validator

--- app/controllers/user/userController.js
+++ app/controllers/user/userController.js
@@ -7,9 +7,17 @@ const userController = {
       // je récupère l'email et le mot de passe qui a était envoyé
       const { email, password } = req.body;
       const user = await userServices.CheckUser(email, password);
+
+      if (user === 401) {
+        return res.status(401).send('le nom d\'utilisateur ou le mot de passe ne correspondent pas');
+      }
+      if (user === 400) {
+        return res.status(400).send('le format de l\'email n\'est pas correct');
+      }
       res.json(user);
     } catch (error) {
       console.error(error);
+      res.status(500).render('erreur lié a la bdd');
     }
   },
 };
--- app/services/userServices.js
+++ app/services/userServices.js
@@ -1,18 +1,30 @@
+const emailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
 const userDatamapper = require('../models/datamappers/userDatamapper');
 
 const userServices = {
-  async CheckUser(email, password, res) {
-    const user = await userDatamapper.getOneUser(email);
-    if (!user) {
-      const error = "l'utilisateur n'existe pas";
-      console.log('<< 401 UNAUTHORIZED');
-      return res.sendStatus(401);
+  async CheckUser(email, password) {
+    // je prépare une variable en cas d'erreur
+    let error = 401;
+    // si l'email n'est pas au bon format alors je modifie le code erreur et le renvoie au controlleur
+    if (!emailValidator.validate(email)) {
+      error = 400;
+      return error;
     }
-    if (password === user.password) {
+    // sinon je cherche l'user dans la bdd en fonction de l'email qui m'a etais fourni
+    const user = await userDatamapper.getOneUser(email);
+    // si je n'est pas d'email alors je renvoie le code d'erreur
+    if (!user) {
+      return error;
+    }
+    // je compare le mot de passe avec bcrypt et celui stocker en bdd qui est crypté
+    const isMatchingPassword = await bcrypt.compare(password, user.password);
+    // si ça matche alors je supprime le mot de passe du json et le renvoie au controler
+    if (isMatchingPassword) {
       Reflect.deleteProperty(user, 'password');
       return user;
     }
-    const error = "le mot de passe n'est pas correct";
+    // sinon je renvoie erreur 401 au controller
     return error;
   },
 
