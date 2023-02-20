9f301877f783922965304ab509232470850b6fc1
alexandre-cabarez <alexandre.cabarez@gmail.com>
2023-02-20 17:13:27 +0100

    mise en place jwt test -> ok

--- app/services/userServices.js
+++ app/services/userServices.js
@@ -1,31 +1,41 @@
 const emailValidator = require('email-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userDatamapper = require('../models/datamappers/userDatamapper');
+const config = require('../../configJwt');
 
 const userServices = {
   async CheckUser(email, password) {
-    // je prépare une variable en cas d'erreur
-    let error = 401;
-    // si l'email n'est pas au bon format alors je modifie le code erreur et le renvoie au controlleur
+    // l'email n'est pas au bon format alors je modifie le code erreur et le renvoie
     if (!emailValidator.validate(email)) {
-      error = 400;
-      return error;
+      return 400;
     }
     // sinon je cherche l'user dans la bdd en fonction de l'email qui m'a etais fourni
     const user = await userDatamapper.getOneUser(email);
     // si je n'est pas d'email alors je renvoie le code d'erreur
     if (!user) {
-      return error;
+      return 401;
     }
     // je compare le mot de passe avec bcrypt et celui stocker en bdd qui est crypté
     const isMatchingPassword = await bcrypt.compare(password, user.password);
     // si ça matche alors je supprime le mot de passe du json et le renvoie au controler
     if (isMatchingPassword) {
       Reflect.deleteProperty(user, 'password');
-      return user;
+      const token = jwt.sign({
+        email: user.email,
+        id_role: user.id_role,
+        phone: user.phone,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        address: user.address,
+        zip_code: user.zip_code,
+        city: user.city,
+      }, config.jwtSecret, { expiresIn: '1h' });
+
+      return token;
     }
     // sinon je renvoie erreur 401 au controller
-    return error;
+    return 401;
   },
 
 };
