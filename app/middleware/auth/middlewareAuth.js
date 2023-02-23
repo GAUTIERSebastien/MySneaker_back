62f13bd5bae6265bc1e23c5eee341f30225e890c
alexandre-cabarez <alexandre.cabarez@gmail.com>
2023-02-23 10:02:23 +0100

    correction middlewareAuth

--- app/middleware/auth/middlewareAuth.js
+++ app/middleware/auth/middlewareAuth.js
@@ -1,26 +1,24 @@
 const express = require('express');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const middlewareAuth = {
   isLogged(req, res, next) {
-    verifyToken(req, res, next) {
-      const authHeader = req.headers.token;
-      if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        try {
-          const decoded = jwt.verify(token, process.env.SECRET_JWT );
-          console.log(decoded)
-          req.user = decoded;
-          next();
-        } catch (err) {
-          res.status(401).json({ error: 'Token invalide' });
-        }
-      } else {
-        res.status(401).json({ error: 'Token manquant' });
+    const authHeader = req.headers.token;
+    if (authHeader) {
+      const token = authHeader.split(' ')[1];
+      try {
+        const decoded = jwt.verify(token, process.env.SECRET_JWT);
+        console.log(decoded);
+        req.user = decoded;
+        next();
+      } catch (err) {
+        res.status(401).json({ error: 'Token invalide' });
       }
+    } else {
+      res.status(401).json({ error: 'Token manquant' });
     }
   },
-
 };
 
 module.exports = middlewareAuth;
