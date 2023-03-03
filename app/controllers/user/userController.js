45580726ce9ce5b862dc437e67f04107f352f8e8
Sébastien GAUTIER <sgautier034@gmail.com>
2023-03-03 11:46:07 +0100

    Fix :spelling

--- app/controllers/user/userController.js
+++ app/controllers/user/userController.js
@@ -13,14 +13,14 @@ const userController = {
       const user = await userServices.CheckUser(email, password);
       // si une l'utilisateur n'existe pas dans la bdd une erreur est envoyé
       if (user === 401) {
-        return res.status(401).send('le nom d\'utilisateur ou le mot de passe ne correspondent pas');
+        return res.status(401).send('Le nom d\'utilisateur ou le mot de passe ne correspondent pas');
       }
       // si l'utilisateur a renseigné une adress email invalide un message d'erreur est renvoyé
       if (user === 400) {
-        return res.status(400).send('le format de l\'email n\'est pas correct');
+        return res.status(400).send('Le format de l\'email n\'est pas correct');
       }
       if (user === 402) {
-        return res.status(401).send('votre compte est désactivé');
+        return res.status(401).send('Votre compte est désactivé');
       }
       // si tout est correcte on créer un token jwt
       const token = jwt.sign({
@@ -54,10 +54,10 @@ const userController = {
       const result = await userServices.CheckUserAndAdd(newUser);
       if (result === 200) {
         // si tous c'est bien passé je renvoie un code 200 avec un message
-        res.status(200).send('l\'utilisateur est bien enregistré');
+        res.status(200).send('L\'utilisateur est bien enregistré');
       } else {
         // si des information sont incorrects je renvoie un code 400 avec un message
-        res.status(400).send('les données saisie sont incorrect');
+        res.status(400).send('Les données saisie sont incorrect');
       }
       // si la bdd ne répond pas ou que les données saisie ne peuvent pas etre rentré dans la bdd
       // alors je renvoie un code 500 avec le log de l'erreur
@@ -73,10 +73,10 @@ const userController = {
     const idUser = req.user.id;
     const result = userServices.modifyUser(user, idUser);
     if (result === 404) {
-      res.status(404).send('cette utilisateur n\'existe pas');
+      res.status(404).send('L\'utilisateur n\'existe pas');
     }
     if (result === 400) {
-      res.status(400).send('les données saisies sont incorrectes');
+      res.status(400).send('Les données saisies sont incorrectes');
     }
     res.send('test ok');
   },
@@ -86,9 +86,9 @@ const userController = {
     try {
       const result = userServices.delete(idUser);
       if (result === 400) {
-        res.status(400).send('impossible de supprimer l\'utilisateur');
+        res.status(400).send('Impossible de supprimer l\'utilisateur');
       }
-      res.status(200).send('l\'utilisateur est bien supprimer');
+      res.status(200).send('L\'utilisateur est bien supprimer');
     } catch (error) {
       res.status(500).send(error);
     }
@@ -99,9 +99,9 @@ const userController = {
     try {
       const result = await userDatamapper.hiddenUser(idUser);
       if (result === 400) {
-        res.status(400).send('impossible de supprimer l\'utilisateur');
+        res.status(400).send('Impossible de supprimer l\'utilisateur');
       }
-      res.status(200).send('l\'utilisateur est bien supprimer');
+      res.status(200).send('L\'utilisateur est bien supprimer');
     } catch (error) {
       res.status(500).send(error);
     }
